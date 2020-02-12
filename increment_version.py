c65c43f18b7e0f66c38a9f5d0f36bdeb1f904bb5
Billy Sheppard <39789405+Billy-Sheppard@users.noreply.github.com>
2020-02-12 11:46:24 +1100

    updated

--- increment_version.py
+++ increment_version.py
@@ -17,19 +17,19 @@ with open ("Cargo.toml", "r") as cargo_toml:
     patch = ver_num[ver_num.find(".", ver_num.find(".")+1)+1:]    
     old_ver = str(major) + "." + str(minor) + "." + str(patch)                 # record old version
 
-    if sys.argv[1] == '-m':                                                    # if major increase
+    if sys.argv[1] == '-m' or sys.argv[1] == '-major':                         # if major increase
         major = int(major) + 1
+        minor = 0
+        patch = 0
         # print(new_major)
-        new_ver = str(major) + "." + str(minor) + "." + str(patch)             # construct new version string
-    elif sys.argv[1] == '-n':                                                  # if minor increase
+    elif sys.argv[1] == '-n' or sys.argv[1] == '-minor':                       # if minor increase
         minor = int(minor) + 1
+        patch = 0
         # print(new_minor)
-        new_ver = str(major) + "." + str(minor) + "." + str(patch)             # construct new version string
-    elif sys.argv[1] == '-p':                                                  # if patch increase
+    elif sys.argv[1] == '-p' or sys.argv[1] == '-patch':                       # if patch increase
         patch = int(patch) + 1
         # print(new_patch)
-        new_ver = str(major) + "." + str(minor) + "." + str(patch)             # construct new version string
-    elif sys.argv[1] == '-v':                                                  # if version declared
+    elif sys.argv[1] == '-v' or sys.argv[1] == "-version":                     # if version declared
         if re.match(regex, sys.argv[2]): 
             new_ver = sys.argv[2]
         else :
@@ -39,10 +39,10 @@ with open ("Cargo.toml", "r") as cargo_toml:
         print("Incorrect argument. Cancelling...")
         sys.exit()
 
-    
+      
     new_file = file.replace(old_ver, new_ver)                                    # replace old version string
     cargo_file = open("Cargo.toml", "w")                                        # open cargo file for editing
     cargo_file.write(new_file)                                                   # write new files contents
     cargo_file.close()                                                          # close writer
 
-    print("Updated from: " + old_ver + " to: " + new_ver)                      # log increment
+    print("Updated version string from: " + old_ver + " to: " + new_ver)       # log increment
